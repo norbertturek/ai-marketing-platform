695d84810dcd7d558b2526f5a2971f7d0cb26b6a
Norbert Turek <norbert.turek@parkapp.pl>
2026-03-03 13:54:47 +0100

    fix(api): import PrismaModule in CreditsModule to resolve DI error (#21)
    CreditsRepository depends on PrismaService which requires PrismaModule
    to be imported (it is not @Global). This caused a runtime crash on
    deploy: "Nest can't resolve dependencies of CreditsRepository".

--- apps/api/src/credits/credits.module.ts
+++ apps/api/src/credits/credits.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
 import { CreditsController } from './credits.controller';
 import { CreditsRepository } from './credits.repository';
 import { CreditsService } from './credits.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CreditsController],
   providers: [CreditsRepository, CreditsService],
   exports: [CreditsService],
